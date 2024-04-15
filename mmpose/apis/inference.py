1df9c7db0f8d3770eefdeecc94fc84289bd7bc3b
Niels <niels.rogge1@gmail.com>
2024-04-15 15:20:52 +0200

    Save person_results

--- mmpose/apis/inference.py
+++ mmpose/apis/inference.py
@@ -391,6 +391,17 @@ def inference_top_down_pose_model(model,
     # Change for-loop preprocess each bbox to preprocess all bboxes at once.
     bboxes = np.array([box['bbox'] for box in person_results])
 
+    # save to hf_hub
+    torch.save(person_results, "vitpose_person_results.pt")
+    from huggingface_hub import HfApi
+    api = HfApi()
+    api.upload_file(
+        path_or_fileobj="vitpose_person_results.pt",
+        path_in_repo="vitpose_person_results.pt",
+        repo_id="nielsr/test-image",
+        repo_type="dataset",
+    )
+
     # Select bboxes by score threshold
     if bbox_thr is not None:
         assert bboxes.shape[1] == 5
